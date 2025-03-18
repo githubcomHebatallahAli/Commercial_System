11ec99e7297f1f2b94aed6fc0512bffb97b4a809
Hebatallah Ali <heba.111980.ma@gmail.com>
2025-03-18 22:43:27 +0200

    update

--- app/Http/Controllers/Admin/StatisticsController.php
+++ app/Http/Controllers/Admin/StatisticsController.php
@@ -11,31 +11,6 @@ use App\Http\Controllers\Controller;
 
 class StatisticsController extends Controller
 {
-    // public function showStatistics()
-    // {
-    //     $this->authorize('manage_users');
-    //     $productsCount = Product::count();
-    //     $categoriesCount = Category::count();
-    //     $invoicesCount = Invoice::count();
-    //     $sales = Invoice::sum('invoiceAfterDiscount');
-    //     $netProfit = Invoice::sum('profit');
-    //     $totalWithdrawals = Withdraw::sum('withdrawnAmount');
-    //     $availableWithdrawal = $sales - $totalWithdrawals;
-
-
-    //     $statistics = [
-    //         'Categories_count' => $categoriesCount,
-    //         'Products_count' => $productsCount,
-    //         'Invoices_count' => $invoicesCount,
-    //         'Sales' => $sales,
-    //         'Net_Profit' => $netProfit,
-    //         'Available_Withdrawal' => $availableWithdrawal,
-
-    //     ];
-
-    //     return response()->json($statistics);
-    // }
-
     public function showStatistics()
 {
     $this->authorize('manage_users');
